5d3b08a6d0e9926078606f96f253107681442726
Tanausu Sosa <tanausu_sa@hotmail.com>
2021-05-31 14:45:45 +0400

    feat: ensure server starts after connecting to mongodb

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,10 @@
 import 'module-alias/register'
 import env from './config/env'
-import app from './config/app'
+import { MongoHelper } from '@/main/helpers'
 
-app.listen(env.port, () => console.log(`Server running on ${env.port}`))
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () => console.log(`Server running on ${env.port}`))
+  })
+  .catch(console.error)
