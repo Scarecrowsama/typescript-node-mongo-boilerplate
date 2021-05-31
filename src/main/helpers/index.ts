2c5ea8d0e92896fa198cf305ee449b3d52dd5c61
Tanausu Sosa <tanausu_sa@hotmail.com>
2021-05-31 18:17:40 +0400

    feat: add MongoQueryBuilder

--- src/main/helpers/index.ts
+++ src/main/helpers/index.ts
@@ -1,1 +1,2 @@
 export * from './mongo-helper'
+export * from './mongo-query-builder'
---
+++ src/main/helpers/mongo-query-builder.ts
@@ -0,0 +1,56 @@
+export class MongoQueryBuilder {
+  private readonly query = []
+
+  match (data: object): MongoQueryBuilder {
+    this.query.push({
+      $match: data
+    })
+    return this
+  }
+
+  group (data: object): MongoQueryBuilder {
+    this.query.push({
+      $group: data
+    })
+    return this
+  }
+
+  unwind (data: object): MongoQueryBuilder {
+    this.query.push({
+      $unwind: data
+    })
+    return this
+  }
+
+  lookup (data: object): MongoQueryBuilder {
+    this.query.push({
+      $lookup: data
+    })
+    return this
+  }
+
+  addFields (data: object): MongoQueryBuilder {
+    this.query.push({
+      $addFields: data
+    })
+    return this
+  }
+
+  sort (data: object): MongoQueryBuilder {
+    this.query.push({
+      $sort: data
+    })
+    return this
+  }
+
+  project (data: object): MongoQueryBuilder {
+    this.query.push({
+      $project: data
+    })
+    return this
+  }
+
+  build (): object[] {
+    return this.query
+  }
+}
